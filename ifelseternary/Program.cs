ea49aa7bfb0c3ed847db2760d02b75fc45d91a92
Yasin <98458144+yesyu2@users.noreply.github.com>
2023-04-10 15:20:15 +0300

    ifelseternary

---
+++ ifelseternary/Program.cs
@@ -0,0 +1,13 @@
+﻿int time = DateTime.Now.Hour;
+if (time>6 && time<11)
+    Console.WriteLine("Good Morning!");
+else if (time<=18)
+    Console.WriteLine("İyi günler!");
+else
+    Console.WriteLine("Good Night!)");
+
+string sonuc = time<=18 ? "Have a nice day!" : "Good Night!";
+Console.WriteLine(sonuc);
+
+sonuc = time>=6 && time<11 ? "Good Morning!": time <= 18 ? "Have a nice day" : "Good Night!";
+Console.WriteLine(sonuc);
